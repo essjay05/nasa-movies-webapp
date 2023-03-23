89a8a1751a4a0b75abb5ff292d898b7656d544f4
Joy Serquiña <stepjoyous@gmail.com>
2023-03-23 16:43:16 -0700

    Successfully retrieved NASA pod image and info. Applied basic structure to NASAImg component. wip: retrieving movie data.

--- src/components/NASAImg/NASAImg.js
+++ src/components/NASAImg/NASAImg.js
@@ -1,30 +1,46 @@
 import { useState, useEffect } from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 
 import './NASAImg.styles.scss'
 
 const NASAImg = () => {
 
   const [ loading, setLoading ] = useState(true)
-  const [ nasaImg, setNasaImg ] = useState('')
+  const [ data, setData ] = useState(null)
 
-  // const nasa
+
+
 
   useEffect(() => {
-    // axios(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_APOD_KEY}`)
-    console.log(`nasaImg: ${nasaImg}`)
-    console.log(process.env.REACT_APP_NASA_APOD_KEY)
-  }, [nasaImg])
+    axios(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_APOD_KEY}`)
+      .then(response => {
+        setData(response.data)
+        console.log(`axios.then response.data`)
+        console.log(response.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  
+  }, [])
 
+  const { copyright, date, explanation, hdurl } = {...data}
 
   return (
-    <section className='nasaImgSection'>
+    <section className='nasa-img-section'>
       { loading ? 
         <h2>Loading...</h2>
       :
         <>
-          <h2><span>NASA:</span> Picture of the Day</h2>
-          <>{nasaImg}</>
+          <h2 className='nasa-img-h2'><span>NASA:</span> Picture of the Day</h2>
+          <h3 className='nasa-pod-date'>{date}</h3>
+          <figure className='nasa-img-wrapper mw-75'>
+            <img src={hdurl} alt={explanation} className='nasa-img mw-75' />
+            <figcaption className='nasa-img-details'>Copyright: {copyright}</figcaption>
+          </figure>
         </>
       }
     </section>
