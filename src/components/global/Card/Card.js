f4d82f28eb68ec458c92f22f3e139531f643153e
Joy Serquiña <stepjoyous@gmail.com>
2023-03-23 23:48:10 -0700

    Successfully passed along TMDB api results and mapped over Card data to produce data retrieved. wip: filtering results to relevant to NASA and refactoring mapping over Cards into MoviesList component.

--- src/components/global/Card/Card.js
+++ src/components/global/Card/Card.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
-import './Card.scss'
+import './Card.styles.scss'
 
-const Card = () => {
+const Card = ({ props, item, id, imgSrc, title, description, releaseDate, popularity }) => {
+
+  console.log('Card item:')
+  console.log(item)
   return (
     <div className='card'>
-      <img
-        src=''
-        alt='Movie Poster' 
-        className='card-img-top' />
+      { imgSrc !== null ?
+        <img
+          src={`https://image.tmdb.org/t/p/original/${imgSrc}`}
+          alt={`Movie Poster for ${title}`}
+          className='card-img-top' />
+      :
+        <h2>No poster found</h2>
+      }
       <div className='card-body'>
-        <h4 className='card-title'>Movie Title</h4>
-        <p className='card-description'>Description goes here....</p>
+        <h4 className='card-title'>{title}</h4>
+        <p className='card-description'>{description}</p>
+        <ul>
+          <li>
+            <strong>Release Date: </strong>{releaseDate}
+          </li>
+          <li>
+            <strong>Popularity: </strong>{popularity}
+          </li>
+        </ul>
         <button className='card-btn' href=''>Link to Movie Detail</button>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import NASAImg from '../components/NASAImg/NASAImg'
-import MoviesList from '../components/MoviesList/MoviesList'
+// import MoviesList from '../components/MoviesList/MoviesList'
+import Card from '../components/global/Card/Card'
 
 const Home = ({ pageName }) => {
 
@@ -12,9 +13,9 @@ const Home = ({ pageName }) => {
   useEffect(() => {
     axios(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&query=NASA&include_adult=false`)
       .then(response => {
-        setData(response.data)
+        setData(response.data.results)
         console.log(`HOME axios.then response.data:`)
-        console.log(response.data)
+        console.log(response.data.results)
       })
       .catch(err => {
         console.error(err)
@@ -28,14 +29,25 @@ const Home = ({ pageName }) => {
     <main>
       <h1>NASA Movies Web App: {pageName}</h1>
       <NASAImg/>
-      { loading && !data ?
-        <h2>Loading...</h2>
+      { !loading && data ?
+        <section className='d-flex flex-wrap'>
+          {data.map(movie => {
+            const { id, poster_path, title, overview, release_date, popularity } = {...movie}
+            return (
+              <Card 
+                key={id}
+                imgSrc={poster_path}
+                title={title}
+                description={overview}
+                releaseDate={release_date}
+                popularity={popularity}/>
+            )
+          })}
+        </section>
       :
-        <>
-          <h2>Data is loaded!</h2>
-        </>
+        <h2>Loading...</h2>
       }
-      <MoviesList/>
+      
     </main>
   )
 }
