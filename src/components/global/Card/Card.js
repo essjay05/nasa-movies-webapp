2a020d3e0d50bf28fc283ef1d7ad56efb5a1ac03
Joy Serquiña <stepjoyous@gmail.com>
2023-03-23 20:48:27 -0700

    Created generic hard-coded Card global component to be used for MoviesList. wip: MoviesList component.

--- src/components/global/Card/Card.js
+++ src/components/global/Card/Card.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import './Card.scss'
+
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <img
+        src=''
+        alt='Movie Poster' 
+        className='card-img-top' />
+      <div className='card-body'>
+        <h4 className='card-title'>Movie Title</h4>
+        <p className='card-description'>Description goes here....</p>
+        <button className='card-btn' href=''>Link to Movie Detail</button>
+      </div>
+    </div>
   )
 }
 
