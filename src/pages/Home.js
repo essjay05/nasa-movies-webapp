9aaf3884f36da958e20dfc3fb52691b62a1c44d7
Joy Serquiña <stepjoyous@gmail.com>
2023-03-25 01:36:53 -0700

    Cleaned up the code to make it more readable for Home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,9 @@ const Home = ({ pageName }) => {
 
   return (
     <main>
-      <h1 className='text-center my-3 w-100'>NASA Movies Web App: {pageName}</h1>
+      <h1 className='text-center my-3 w-100'>
+        NASA Movies Web App: {pageName}
+      </h1>
       <NASAImg/>
       <MoviesList/>
     </main>
